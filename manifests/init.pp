a43f313cdd20f43dff69a03c5bd106a2c9cdcc1c
Adam Breaux <breauxaj@gmail.com>
2013-08-19 11:07:50 -0400

    s/g cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,16 @@
 class tuned {
-    $required = $operatingsystem ? {
-        /(?i-mx:centos|fedora|redhat|scientific)/ => 'tuned',
-    }
+  $required = $operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => 'tuned',
+  }
 
-    package { $required: ensure => latest }
+  package { $required: ensure => latest }
 
 }
 
 define tuned::profile {
-    exec { tuned-profile:
-        path => '/usr/bin',
-        command => "tuned-adm profile ${name}",
-    }
+  exec { tuned-profile:
+    path => '/usr/bin',
+    command => "tuned-adm profile ${name}",
+  }
 
 }
